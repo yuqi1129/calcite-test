5e9af80b2b5d6644dcc1ff27fcdc647b6c90acb1
余启 <yuqi5@xiaomi.com>
2019-05-10 17:55:30 +0800

    remove unused code in ParserTestOne

--- src/main/java/com/netease/yuqi/parser/ParserTestOne.java
+++ src/main/java/com/netease/yuqi/parser/ParserTestOne.java
@@ -24,7 +24,6 @@ public class ParserTestOne {
 
 
     public static void main(String[] args) {
-        SchemaPlus rootSchema = Frameworks.createRootSchema(true);
         final FrameworkConfig config = Frameworks.newConfigBuilder()
                 .parserConfig(SqlParser.configBuilder()
                         .setParserFactory(SqlParserImpl.FACTORY)
@@ -36,26 +35,6 @@ public class ParserTestOne {
                         .build())
                 .build();
 
-        /**
-        rootSchema.add("test_table", new AbstractTable() {
-            @Override
-            public RelDataType getRowType(RelDataTypeFactory typeFactory) {
-                RelDataTypeFactory.FieldInfoBuilder builder = typeFactory.builder();
-                RelDataType intType = typeFactory.createTypeWithNullability(
-                        typeFactory.createSqlType(SqlTypeName.INTEGER), true);
-                RelDataType stringType = typeFactory.createTypeWithNullability(
-                        typeFactory.createSqlType(SqlTypeName.VARCHAR), true);
-
-
-
-                builder.add("id", intType);
-                builder.add("name", stringType);
-
-                return builder.build();
-            }
-        });
-
-         */
         String sql = "select ids, name from test where id < 5 and name = 'zhang'";
         SqlParser parser = SqlParser.create(sql, config.getParserConfig());
         try {
